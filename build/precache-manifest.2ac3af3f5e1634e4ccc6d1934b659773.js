21bae16f12dd98c790da08a624e7dc99dd8521a4
aparna <aparna.chinnaiah@hipbar.com>
2020-01-26 11:12:16 +0530

    CRA setup done

---
+++ build/precache-manifest.2ac3af3f5e1634e4ccc6d1934b659773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb8c99f46914b7bff2b4aefac243aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "953eb4b3fd988185c358",
+    "url": "/static/css/main.6243ec94.chunk.css"
+  },
+  {
+    "revision": "7afdb0f8863705747a3d",
+    "url": "/static/js/2.de30b1bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.de30b1bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "953eb4b3fd988185c358",
+    "url": "/static/js/main.9cda2c9d.chunk.js"
+  },
+  {
+    "revision": "510eae0d601bbeb4b90d",
+    "url": "/static/js/runtime-main.f1c36703.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
